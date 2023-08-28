2839787ab5e32f645d79d68ec18aa9dab1614f09
chaitanyadav <94705423+chaitanyadav@users.noreply.github.com>
2023-08-28 12:13:20 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -5,8 +5,8 @@ const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 passport.use(new GoogleStrategy({
-  clientID: '567790212408-bge0rkkh8qirc0h5nblgtv459vcp5443.apps.googleusercontent.com',
-  clientSecret: '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',
+  clientID: 'enter your id',
+  clientSecret: 'enter details',
   callbackURL: "http://localhost:3000/auth/google/callback",
   passReqToCallback: true,
 },
